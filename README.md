587f48e9a50ebdbdf80dcaa14fdac78e27574139
Abhishek Panda <abhishek.panda@flipkart.com>
2019-09-10 13:28:06 +0530

    VSCode setup for C/C++
    VSCode setup for C/C++

---
+++ README.md
@@ -0,0 +1,1 @@
+This repo contains vscode set for programming environment
---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<math.h>
+
+int main () {
+    int a = 5;
+    printf("Hello world from C %f", sqrt(a));
+    return 0;
+}
